7be5f2e335c02fcc39a2228d29171ee9bfb8462f
SohagHossen <sohaghossen.cse@gmail.com>
2024-09-14 22:45:49 +0600

    add new program using bool function

---
+++ bool.py
@@ -0,0 +1,16 @@
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
