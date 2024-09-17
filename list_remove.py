81b5a99aeb251b3bea790c41b8460f3bbdf3d9a2
SohagHossen <sohaghossen.cse@gmail.com>
2024-09-17 23:08:31 +0600

    add new code for remove item from list using python code

---
+++ list_remove.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.pop(1)
+print(thislist)
+thislist.pop()
+print(thislist)
+del thislist[0]
+print(thislist)
+thislist.remove("banana")
+print(thislist)
+del thislist
