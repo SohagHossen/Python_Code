dcc808c72945935d9b1903e54292fdd52fda282f
SohagHossen <sohaghossen.cse@gmail.com>
2024-09-24 22:52:39 +0600

    add new program using tuple assign

---
+++ tuple_assign.py
@@ -0,0 +1,11 @@
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+#print first index
+print(thistuple[1])
+#print last index
+print(thistuple[-1])
+#print 2nd index to last index
+print(thistuple[2:])
+#print 1nd index to 4 index
+print(thistuple[:4])
+#print 2nd index to 5 index
+print(thistuple[2:5])
