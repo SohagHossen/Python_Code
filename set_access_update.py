eabb3e0979659d51ba9a0407005490c42686c563
SohagHossen <sohaghossen.cse@gmail.com>
2024-10-01 23:03:53 +0600

    access and updat on sets using python

---
+++ set_access_update.py
@@ -0,0 +1,21 @@
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+
+# access sets using python
+for x in set1:
+  print(x)
+  
+#add new valu add in Execting  set
+  
+set2.add("orange")
+
+print(set2)
+
+set3.update("update")
+
+print(set3)
