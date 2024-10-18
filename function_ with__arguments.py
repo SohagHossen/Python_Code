a51c41a482978c48eb4bbbc2e1eef63fc808725f
SohagHossen <sohaghossen.cse@gmail.com>
2024-10-18 22:13:16 +0600

    add function with and without arguments using python

---
+++ function_ with__arguments.py
@@ -0,0 +1,12 @@
+#function with arguments
+
+def my_function(fname):
+  print("Hi, My name is: "+fname)
+
+#function without arguments  
+def my_func():
+	print("We are learning Devops")
+
+my_function("sohag")
+my_function("saimum")
+my_func()
