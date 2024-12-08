cf74958922ec42a283d6178ea4ebbb735282b535
Sohag Hossen <34201824+SohagHossen@users.noreply.github.com>
2024-12-08 23:14:29 +0600

    add  inheritance super function using python

---
+++ inheritance_super_function.py
@@ -0,0 +1,17 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+x = Student("Mike", "Olsen")
+print("First Name:", x.firstname)
+print("Last Name: ", x.lastname)
+print("Passing Year: ", x.graduationyear)
